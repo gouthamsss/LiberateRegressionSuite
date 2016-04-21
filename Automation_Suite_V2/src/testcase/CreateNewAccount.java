6fa522d36cd300f7f2eab5e324e068dd02905da5
Nikhil Das <nikhil.thottupurathu@igate.com>
2016-04-21 16:39:25 +0530

    Started Create New Account Function

---
+++ Automation_Suite_V2/src/testcase/CreateNewAccount.java
@@ -0,0 +1,13 @@
+package testcase;
+
+import operations.TestData;
+
+public class CreateNewAccount
+{
+	String AccountNumber;
+	String AccountFirstName = TestData.customerName();
+	String AccountLastName	= TestData.customerName();
+	String ID;
+	
+	
+}
