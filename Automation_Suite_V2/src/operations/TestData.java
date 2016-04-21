7bd39d43506be680551d67d582410d0ede8fa168
Nikhil Das <nikhil.thottupurathu@igate.com>
2016-04-21 16:38:36 +0530

    0.1.0 : Added : Method to generate random string

--- Automation_Suite_V2/src/operations/TestData.java
+++ Automation_Suite_V2/src/operations/TestData.java
@@ -1,7 +1,19 @@
 package operations;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+
 public class TestData
 {
-	String testDataClassVersion = "0.0.0 : Added new TestData class";
-
+	String testDataClassVersion = "0.1.0 : Added : Method to generate random string";
+	
+	public static SecureRandom random = new SecureRandom();
+	
+	public static String customerName()
+	{
+		String customerName = new BigInteger(130, random).toString(32);
+		customerName = customerName.substring(0,12);
+		
+		return customerName;
+	}
 }
