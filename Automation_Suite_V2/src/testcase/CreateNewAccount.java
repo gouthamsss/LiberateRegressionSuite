509b24a2eb8301d7cfe84a14b5f84540561162dd
Nikhil Das <nikhil.thottupurathu@igate.com>
2016-05-02 14:16:42 +0530

    Started formating code

--- Automation_Suite_V2/src/testcase/CreateNewAccount.java
+++ Automation_Suite_V2/src/testcase/CreateNewAccount.java
@@ -7,146 +7,145 @@ import operations.TestReport;
 
 public class CreateNewAccount
 {
-        static TestReport report = new TestReport("CreateNewAccount", "");
+	TestReport report = new TestReport("CreateNewAccount", "");
 
-        static String AccountNumber;
-        static String AccountFirstName = TestData.randomString();
-        static String AccountLastName = TestData.randomString();
-        //String CustomerID;
-        static String ApplicationForm = "fixedResidential";
-        static String CustomerType = "R";
-        static String ServiceType = "PEL";
-        static String ServicePackage = "ETFTESTING";
-        static String CustomeIDType = "NI";
-        static String Company = "CYN";
-        static String MarketingCategory = "990000";
-        static String Region = "CAY";
-        static String AccountType = "TP";
-        static String BillStatusArea = "BOT";
+	String AccountNumber;
+	String AccountFirstName = TestData.randomString();
+    String AccountLastName = TestData.randomString();
+    //String CustomerID;
+    String ApplicationForm = "fixedResidential";
+    String CustomerType = "R";
+    String ServiceType = "PEL";
+    String ServicePackage = "ETFTESTING";
+    String CustomeIDType = "NI";
+    String Company = "CYN";
+    String MarketingCategory = "990000";
+    String Region = "CAY";
+    String AccountType = "TP";
+    String BillStatusArea = "BOT";
 
-        static boolean SelectServicePackage = true;
-        static boolean CustomerIDProvided = false;
+    boolean SelectServicePackage = true;
+    boolean CustomerIDProvided = false;
 
-        static boolean passed;
+    boolean passed;
 
-        static String xpath;
-        static String xpath_1;
-        static String xpath_2;
+    String xpath;
+    String xpath_1;
+    String xpath_2;
 
-        static TestAction ta = new TestAction(Operations.getdriver());
+    TestAction ta = new TestAction(Operations.getdriver());
 
-        public static void execute()
+    public  void execute()
+    {
+    	//LoginLogout.login("http://172.21.73.80:8081/liberate-LONI01-S10/");
+        LoginLogout.login("http://172.21.73.80:8083/liberate-LONI02-S06/");
+
+        while (true)
         {
-            //LoginLogout.login("http://172.21.73.80:8081/liberate-LONI01-S10/");
-            LoginLogout.login("http://172.21.73.80:8083/liberate-LONI02-S06/");
-
-            while (true)
-            {
-                passed = testStep_1();
-                report.takeScreenshot();
-                if (!passed)
+        	passed = testStep_1();
+            report.takeScreenshot();
+            if (!passed)
+            	break;
+            passed = testStep_2();
+            report.takeScreenshot();
+            if (!passed)
                     break;
-                passed = testStep_2();
-                report.takeScreenshot();
-                if (!passed)
+            passed = testStep_3();
+            report.takeScreenshot();
+            if (!passed)
                     break;
-                passed = testStep_3();
-                report.takeScreenshot();
-                if (!passed)
+            passed = testStep_4();
+            report.takeScreenshot();
+            if (!passed)
                     break;
-                passed = testStep_4();
-                report.takeScreenshot();
-                if (!passed)
+            passed = testStep_5();
+            report.takeScreenshot();
+            if (!passed)
                     break;
-                passed = testStep_5();
-                report.takeScreenshot();
-                if (!passed)
-                    break;
-                passed = testStep_6();
-                report.takeScreenshot();
-                if (!passed)
+            passed = testStep_6();
+            report.takeScreenshot();
+            if (!passed)
                     break;
 
-                report.createScreenshotDocument();
-                break;
-            }
-
-            ta.closeSession();
+            report.createScreenshotDocument();
+            break;
         }
+        ta.closeSession();
+	}
 
-        private static boolean testStep_1()
+	private  boolean testStep_1()
+    {
+    	xpath = "//*[text()[contains(.,'Customer Care')]]";
+        passed = ta.waitUntil(xpath, 5);
+        passed = ta.clickOn(xpath);
+
+        return passed;
+	}
+
+    private  boolean testStep_2()
+    {
+    	xpath = "//*[text()[contains(.,'Service Provisioning')]]";
+        passed = ta.waitUntil(xpath, 5);
+        passed = ta.clickOn(xpath);
+
+        return passed;
+	}
+
+    private  boolean testStep_3()
+    {
+    	xpath = "(//*[text()[contains(.,'Application Form:')]]/./following::select)[1]";
+        passed = ta.waitUntil(xpath, 5);
+        ta.closeOKpopup();
+        passed = ta.selectBy(xpath, ApplicationForm);
+
+        if (SelectServicePackage)
         {
-            xpath = "//*[text()[contains(.,'Customer Care')]]";
-            passed = ta.waitUntil(xpath, 5);
-            passed = ta.clickOn(xpath);
+        	xpath = "(//*[text()[contains(.,'Customer Type:')]]/./following::select)[1]";
+            passed = ta.selectBy(xpath, CustomerType);
 
-            return passed;
-        }
+            xpath = "(//*[text()[contains(.,'Service Type:')]]/./following::select)[1]";
+            passed = ta.selectBy(xpath, ServiceType);
 
-        private static boolean testStep_2()
-        {
-            xpath = "//*[text()[contains(.,'Service Provisioning')]]";
-            passed = ta.waitUntil(xpath, 5);
-            passed = ta.clickOn(xpath);
-
-            return passed;
-        }
-
-        private static boolean testStep_3()
-        {
-            xpath = "(//*[text()[contains(.,'Application Form:')]]/./following::select)[1]";
-            passed = ta.waitUntil(xpath, 5);
-            ta.closeOKpopup();
-            passed = ta.selectBy(xpath, ApplicationForm);
-
-            if (SelectServicePackage)
-            {
-                xpath = "(//*[text()[contains(.,'Customer Type:')]]/./following::select)[1]";
-                passed = ta.selectBy(xpath, CustomerType);
-
-                xpath = "(//*[text()[contains(.,'Service Type:')]]/./following::select)[1]";
-                passed = ta.selectBy(xpath, ServiceType);
-
-                xpath = "(//*[text()[contains(.,'Service Package:')]]/./following::select)[1]";
-                ta.waitFor(1000);
+            xpath = "(//*[text()[contains(.,'Service Package:')]]/./following::select)[1]";
+            ta.waitFor(1000);
                 //TODO Below function not yet fixed 
-                passed = ta.waitUntilSelectOptionLoads(xpath, 5);
-                passed = ta.selectBy(xpath, ServicePackage);
-            }
+            passed = ta.waitUntilSelectOptionLoads(xpath, 5);
+            passed = ta.selectBy(xpath, ServicePackage);
+        }
 
-            xpath = "//input[@value='Proceed >>']";
+        xpath = "//input[@value='Proceed >>']";
+        passed = ta.waitUntil(xpath, 2);
+        passed = ta.clickOn(xpath);
+
+        xpath_1 = "(//*[text()[contains(.,'First Name:')]]/./following::input)[1]";
+        xpath_2 = "(//*[text()[contains(.,'ID:')]]/./following::select)[1]";
+
+        xpath = xpath_1 + " | " + xpath_2;
+        passed = ta.waitUntil(xpath, 10);
+
+        if (!ta.elementExist(xpath_1))
+        {
+        	passed = ProvideCustomerID();
+
+            xpath = "(//input[@value='Proceed >>'])[last()]";
+            passed = ta.clickOn(xpath);
+
+            xpath = "//input[@value='Create new account']";
             passed = ta.waitUntil(xpath, 2);
             passed = ta.clickOn(xpath);
-
-            xpath_1 = "(//*[text()[contains(.,'First Name:')]]/./following::input)[1]";
-            xpath_2 = "(//*[text()[contains(.,'ID:')]]/./following::select)[1]";
-
-            xpath = xpath_1 + " | " + xpath_2;
-            passed = ta.waitUntil(xpath, 10);
-
-            if (!ta.elementExist(xpath_1))
-            {
-                passed = ProvideCustomerID();
-
-                xpath = "(//input[@value='Proceed >>'])[last()]";
-                passed = ta.clickOn(xpath);
-
-                xpath = "//input[@value='Create new account']";
-                passed = ta.waitUntil(xpath, 2);
-                passed = ta.clickOn(xpath);
-            }
-
-            return passed;
         }
 
-        private static boolean testStep_4()
+        return passed;
+	}
+
+        private  boolean testStep_4()
         {
             passed = FillApplicationFixedResidential();
 
             return passed;
         }
 
-        private static boolean testStep_5()
+        private  boolean testStep_5()
         {
             xpath = "//*[text()[contains(.,'Account Properties Details')]]";
             passed = ta.waitUntil(xpath,5);
@@ -157,7 +156,7 @@ public class CreateNewAccount
             return passed;
         }
 
-        private static boolean testStep_6()
+        private  boolean testStep_6()
         {
             xpath = "//*[text()='Service Order']";
             passed = ta.waitUntil(xpath, 5);
@@ -171,7 +170,7 @@ public class CreateNewAccount
 
 
         //SUPPORT FUNCTIONS
-        private static boolean FillApplicationFixedResidential()
+        private  boolean FillApplicationFixedResidential()
         {
             //CUSTOMER INFORMATION
             xpath = "(//*[text()[contains(.,'Salutation:')]]/./following::select)[1]";
@@ -300,7 +299,7 @@ public class CreateNewAccount
             return passed;
         }
 
-        private static boolean ProvideCustomerID()
+        private  boolean ProvideCustomerID()
         {
             xpath = "(//*[text()[contains(.,'ID:')]]/./following::select)[1]";
             passed = ta.selectBy(xpath, CustomeIDType);
