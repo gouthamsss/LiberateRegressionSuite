b11ebf324ac60ec3fe147752e3a3b9ae842934e8
Nikhil Das <nikhil.thottupurathu@igate.com>
2016-05-02 16:24:47 +0530

    0.1.1 : Amended : Method nam e to generate random string

--- Automation_Suite_V2/src/operations/TestData.java
+++ Automation_Suite_V2/src/operations/TestData.java
@@ -5,11 +5,11 @@ import java.security.SecureRandom;
 
 public class TestData
 {
-	String testDataClassVersion = "0.1.0 : Added : Method to generate random string";
+	String testDataClassVersion = "0.1.1 : Amended : Method nam e to generate random string";
 	
 	public static SecureRandom random = new SecureRandom();
 	
-	public static String customerName()
+	public static String randomString()
 	{
 		String customerName = new BigInteger(130, random).toString(32);
 		customerName = customerName.substring(0,12);
