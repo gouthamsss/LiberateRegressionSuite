eb36c5611f38b6b7b3b9542f3a22365a829050c3
Nikhil Das <nikhil.thottupurathu@igate.com>
2016-05-03 12:14:07 +0530

    Added conditions to identify screens

--- Automation_Suite_V2/src/testcase/ProvideService.java
+++ Automation_Suite_V2/src/testcase/ProvideService.java
@@ -6,19 +6,20 @@ import operations.TestReport;
 
 public class ProvideService 
 {
-	TestReport report = new TestReport("ProvideService", "");
+	TestReport report = new TestReport("ProvideService", "Reports\\");
 	
     TestAction ta = new TestAction(Operations.getdriver());
 	
-	boolean NewCustomer = false;
-	String AccountNumber= "240004430000";
-	String department	= "BGSAL";
-	String site			= "BUSG";
-    String ServicePackage = "ETFTESTING";
+	boolean NewCustomer 	= false;
+	String AccountNumber	= "240004430000";
+	String department		= "BGSAL";
+	String site				= "BUSG";
+    String ServicePackage	= "ETFTESTING";
 	
     boolean passed;
     String xpath;
-	
+	String screenName;
+    
 	public ProvideService(boolean NewCustomer)
 	{
 		this.NewCustomer = NewCustomer;
@@ -33,6 +34,10 @@ public class ProvideService
 	    {
 	    	passed = testStep_1();
 	        report.takeScreenshot();
+	        if (!passed)
+	        	break;
+	        
+	        passed = testStep_2();
 	        if (!passed)
 	        	break;
 	        
@@ -56,7 +61,7 @@ public class ProvideService
 			xpath = "//input[@value='Search']";
 			passed = ta.clickOn(xpath);
 			
-			xpath = "//*text()[contains(.,'Status:')]";
+			xpath = "//*[text()[contains(.,'Status:')]]";
 			passed = ta.waitUntil(xpath, 3);
 			
 			xpath = "(//*[text()[contains(.,'Department:')]]/./following::select)[1]";
@@ -76,9 +81,33 @@ public class ProvideService
 			
 			xpath = "//input[contains(@value,'Proceed')]";
 			passed = ta.clickOn(xpath);
+			
+			xpath = "//*[text()[contains(.,'Number of Services')]]";
+			passed = ta.waitUntilElementnotExist(xpath, 5);
 		}
 		
 		return passed;
+	}
+	
+	private boolean testStep_2()
+	{
+		while(true)
+		{
+			xpath = "(//*[@class='hdrMid'])[1]";
+			ta.waitUntil(xpath, 10);
+			
+			screenName = ta.getDatafromPage(xpath);
+			if(screenName.equals("Pricing Plans"))
+				pricingPlanScreen();
+			else if(screenName.equals("Service Product"))
+				serviceProducts();
+			else if(screenName.equals("ISP Fields"))
+				ISPFields();
+			else
+				break;
+		}
+		
+		return passed;	
 	}
 	
 	private boolean navigateToExistingCustomerScreen()
@@ -96,6 +125,54 @@ public class ProvideService
     	xpath = "//*[text()[contains(.,'Existing Customer')]]";
         passed = ta.waitUntil(xpath, 5);
         passed = ta.clickOn(xpath);
+		
+		return passed;
+	}
+	
+	private boolean pricingPlanScreen()
+	{
+		xpath = "//*[text()='Pricing Plans']";
+		passed = ta.waitUntil(xpath, 5);
+		
+		//TODO Handle Pricing plan screen
+		
+		xpath = "//input[@value='Proceed']";
+		passed = ta.clickOn(xpath);
+		
+		xpath = "//*[text()='Pricing Plans']";
+		passed = ta.waitUntilElementnotExist(xpath, 5);
+		
+		return passed;
+	}
+	
+	private boolean serviceProducts()
+	{
+		xpath = "//*[text()='Service Product']";
+		passed = ta.waitUntil(xpath, 5);
+		
+		//TODO Handle Service Products screen
+		
+		xpath = "//input[@value='Proceed']";
+		passed = ta.clickOn(xpath);
+		
+		xpath = "//*[text()='Service Product']";
+		passed = ta.waitUntilElementnotExist(xpath, 5);
+		
+		return passed;
+	}
+	
+	private boolean ISPFields()
+	{
+		xpath = "//*[text()='ISP Fields']";
+		passed = ta.waitUntil(xpath, 5);
+		
+		//TODO Handle ISP Fields screen
+		
+		xpath = "//input[@value='Proceed']";
+		passed = ta.clickOn(xpath);
+		
+		xpath = "//*[text()='ISP Fields']";
+		passed = ta.waitUntilElementnotExist(xpath, 5);
 		
 		return passed;
 	}
