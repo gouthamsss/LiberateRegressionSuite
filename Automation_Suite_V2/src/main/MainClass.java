b31825f9f36a197aa79f4a164b2f51b996a21894
Nikhil Das <nikhil.thottupurathu@igate.com>
2016-05-04 17:03:04 +0530

    Moved Test Cases to TestCase class

--- Automation_Suite_V2/src/main/MainClass.java
+++ Automation_Suite_V2/src/main/MainClass.java
@@ -1,26 +1,12 @@
 package main;
 
-import operations.TestReport;
-import testcase.CreateNewAccount;
-import testcase.ProvideService;
 
 public class MainClass
 {
 	public static void main(String [] args)
 	{
-		TestReport report = new TestReport("ProvideService", "Reports\\");
-		CreateNewAccount createNewAccount = new CreateNewAccount(report, "fixedResidential","ETFTESTING");
-		createNewAccount.ServiceType = "PEL";
-		createNewAccount.ServicePackage = "ETFTESTING";
-		createNewAccount.execute();
-
-		ProvideService provideService = new ProvideService(report, createNewAccount.AccountNumber);
-		provideService.NewCustomer = true;
-		provideService.ServiceType = createNewAccount.ServiceType;
-		provideService.ServicePackage = createNewAccount.ServicePackage;
-		provideService.execute();
-		
-		report.createScreenshotDocument();
-
+//		TestCase.PELExistingCustomer();
+//		TestCase.PDLExistingCustomer();
+		TestCase.PELPDLNewCustomer();
 	}
 }
