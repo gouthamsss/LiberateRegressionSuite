b8d2b38171459ec93873585972f00cad0c59ccce
Nikhil Das <nikhil.thottupurathu@igate.com>
2016-05-04 13:38:09 +0530

    Handled PEL provisionoing

--- Automation_Suite_V2/src/testcase/ProvideService.java
+++ Automation_Suite_V2/src/testcase/ProvideService.java
@@ -6,25 +6,46 @@ import operations.TestReport;
 
 public class ProvideService 
 {
-	TestReport report = new TestReport("ProvideService", "Reports\\");
+	TestReport report;
 	
     TestAction ta = new TestAction(Operations.getdriver());
-	
+    SalesSignoff sales; 
+    
 	boolean NewCustomer 	= false;
 	String AccountNumber	= "240004430000";
 	String department		= "BGSAL";
 	String site				= "BUSG";
     String ServicePackage	= "ETFTESTING";
 	String serviceType		= "PEL";
+    String PELExchange		= "BOT";
+    String PELNumberArea	= "BODD";
+    
+    String salesAccNumber	= "";
+    String salesServiceNum	= "";
+    String salesOrderNumber	= "";
     
     boolean passed;
     String xpath;
+    String numberAllocation;
+    boolean fastTrack 	= false;
+    boolean PELPDLProvisioning = false;
 	String oldScreenName = "";
     String newScreenName = "";
 	
-	public ProvideService(boolean NewCustomer)
+    
+	public ProvideService(TestReport report)
 	{
-		this.NewCustomer = NewCustomer;
+		this.report 	= report;
+		NewCustomer 	= true;
+		sales 			= new SalesSignoff(this.report);
+	}
+	
+	public ProvideService(TestReport report, String AccountNumber)
+	{
+		this.AccountNumber = AccountNumber;
+		this.report 	= report;
+		NewCustomer 	= false;
+		sales 			= new SalesSignoff(this.report);
 	}
 	
 	public void execute()
@@ -43,6 +64,19 @@ public class ProvideService
 	        if (!passed)
 	        	break;
 	        
+	        passed = testStep_3();
+	        if(!passed)
+	        	break;
+	        
+	        passed = testStep_4();
+	        if(!passed)
+	        	break;
+	        
+	        passed = testStep_5();
+	        if(!passed)
+	        	break;
+	        
+	        ta.closeSession();
 		    break;
 	    }
 	}
@@ -70,7 +104,7 @@ public class ProvideService
 			passed = ta.selectBy(xpath, department);
 			
 			xpath = "(//*[text()[contains(.,'Site:')]]/./following::select[@disabled = 'disabled'])[1]";
-			passed = ta.waitUntilElementnotExist(xpath, 5);
+			passed = ta.waitUntilElementnotExist(xpath, 2);
 			
 			xpath = "(//*[text()[contains(.,'Site:')]]/./following::select)[1]";
 			passed = ta.selectBy(xpath, site);
@@ -87,6 +121,8 @@ public class ProvideService
 			
 			xpath = "//input[contains(@value,'Proceed')]";
 			passed = ta.clickOn(xpath);
+			
+			report.takeScreenshot();
 			
 			xpath = "//*[text()[contains(.,'Number of Services')]]";
 			passed = ta.waitUntilElementnotExist(xpath, 5);
@@ -124,21 +160,105 @@ public class ProvideService
 			else if (newScreenName.equals("Service Details"))
 				serviceDetails();
 			else
+			{
+				ta.log("ERROR : The screen '"+newScreenName+"' is not handles. Please contact support");
 				break;
+			}
 		}
 		
 		return passed;	
 	}
 	
+	private boolean testStep_3()
+	{
+		long max = 9999999999L;
+		max = (long) (Math.random() * ((max - 1) + 1));
+		
+		xpath = "//*[text()='Contract Details']";
+		passed = ta.waitUntil(xpath, 10);
+		
+		xpath = "(//*[text()[contains(.,'Contract Number:')]]/./following::input)[1]";
+		passed = ta.sendDatatoField(xpath, Long.toString(max));
+		
+		xpath = "(//*[text()[contains(.,'Contract Description:')]]/./following::textArea)[1]";
+		passed = ta.sendDatatoField(xpath, "Contract Description");
+		
+		xpath = "(//*[text()[contains(.,'Contract Duration:')]]/./following::select)[1]";
+		passed = ta.selectBy(xpath, 2);
+		
+		xpath = "//input[@value='Submit']";
+		passed = ta.clickOn(xpath);
+		
+		if(serviceType.equals("PEL"))
+		{
+			xpath = "//*[text()='Do you want to provide Broadband on the same service order']";
+			passed = ta.waitUntil(xpath, 5);
+			
+			report.takeScreenshot();
+			
+			if(!PELPDLProvisioning)
+			{
+				xpath = "//input[@value='No']";
+				passed = ta.waitUntil(xpath, 5);
+				passed = ta.clickOn(xpath);
+			}
+			else
+			{
+				xpath = "//input[@value='Yes']";
+				passed = ta.waitUntil(xpath, 5);
+				passed = ta.clickOn(xpath);
+				//TODO Handle PDL Provisioning
+			}
+		}
+		
+		return passed;
+	}
+	
+	private boolean testStep_4()
+	{
+		passed = sales.signoff();
+		salesAccNumber	= sales.AccountNumber;
+		salesServiceNum	= sales.ServiceNumber;
+		salesOrderNumber= sales.ServiceOrder;
+		
+		if(!AccountNumber.equals(salesAccNumber))
+		{
+			ta.log("ERROR : Mismatch between the AccountNumber provided and Account number in Sales Sign off");
+			passed = false;
+		}
+		
+		return passed;
+	}
+	
+	private boolean testStep_5()
+	{
+		if(!fastTrack)
+		{
+			xpath = "//*[text()='Service Order List']";
+			passed = ta.waitUntil(xpath, 5);
+			
+			report.takeScreenshot();
+			
+			xpath = "//*[text()='"+salesOrderNumber+"']";
+			passed = ta.elementExist(xpath);
+		}		
+		return passed;
+	}
+	
+	//SUPPORTING FUNCTIONS
 	private boolean navigateToExistingCustomerScreen()
 	{
     	xpath = "//*[text()[contains(.,'Customer Care')]]";
         passed = ta.waitUntil(xpath, 5);
         passed = ta.clickOn(xpath);
         
+        report.takeScreenshot();
+        
     	xpath = "//*[text()[contains(.,'Service Provisioning')]]";
         passed = ta.waitUntil(xpath, 5);
         passed = ta.clickOn(xpath);
+        
+        report.takeScreenshot();
         
         ta.closeOKpopup();
         
@@ -146,6 +266,8 @@ public class ProvideService
         passed = ta.waitUntil(xpath, 5);
         passed = ta.clickOn(xpath);
 		
+        report.takeScreenshot();
+        
 		return passed;
 	}
 	
@@ -159,6 +281,8 @@ public class ProvideService
 		
 		xpath = "//input[@value='Proceed']";
 		passed = ta.clickOn(xpath);
+		
+		report.takeScreenshot();
 		
 		xpath = "//*[text()='Pricing Plans']";
 		passed = ta.waitUntilElementnotExist(xpath, 5);
@@ -178,6 +302,8 @@ public class ProvideService
 		xpath = "//input[@value='Proceed']";
 		passed = ta.clickOn(xpath);
 		
+		report.takeScreenshot();
+		
 		xpath = "//*[text()='Service Product']";
 		passed = ta.waitUntilElementnotExist(xpath, 5);
 		
@@ -193,6 +319,8 @@ public class ProvideService
 		
 		xpath = "//input[@value='Proceed']";
 		passed = ta.clickOn(xpath);
+		
+		report.takeScreenshot();
 		
 		xpath = "//*[text()='ISP Fields']";
 		passed = ta.waitUntilElementnotExist(xpath, 5);
@@ -214,6 +342,8 @@ public class ProvideService
 		xpath = "//input[@value='Proceed']";
 		passed = ta.clickOn(xpath);
 		
+		report.takeScreenshot();
+		
 		xpath = "//*[text()='Product Fields']";
 		passed = ta.waitUntilElementnotExist(xpath, 5);
 		
@@ -226,11 +356,81 @@ public class ProvideService
 		passed = ta.waitUntil(xpath, 5);
 		
 		//TODO Handle Service Details screen
+		passed = handleServiceDetailsScreen(serviceType);
 		
 		xpath = "//input[@value='Proceed']";
 		passed = ta.clickOn(xpath);
 		
+		report.takeScreenshot();
+		
 		xpath = "//*[text()='Service Details']";
+		passed = ta.waitUntilElementnotExist(xpath, 5);
+		
+		return passed;
+	}
+	
+	private boolean handleServiceDetailsScreen(String ServiceType)
+	{
+		switch (ServiceType)
+		{
+			case "PEL": passed = PELServiceDetailsScreen();
+				break;
+		}
+		
+		
+		return passed;
+	}
+	
+	/**
+	 * @return
+	 */
+	private boolean PELServiceDetailsScreen()
+	{
+		xpath = "(//*[text()[contains(.,'Exchange:')]]/./following::select)[1]";
+		passed = ta.selectBy(xpath, PELExchange);
+		
+		xpath = "(//*[text()[contains(.,'Number Area:')]]/./following::select[@disabled='disabled'])[1]";
+		passed = ta.waitUntilElementnotExist(xpath, 5);
+		
+		xpath = "(//*[text()[contains(.,'Number Area:')]]/./following::select)[1]";
+		passed = ta.selectBy(xpath, PELNumberArea);
+		
+		xpath = "(//*[text()[contains(.,'Service Number Allocation:')]]/./following::select)[1]";
+		numberAllocation = ta.getValueFromSelect(xpath);
+		
+		if(!numberAllocation.equals("Auto"))
+		{
+			passed = ta.selectBy(xpath, "Auto");
+			
+			xpath = "//input[@value='Deallocate']";
+			passed = ta.waitUntil(xpath, 5);
+		}
+		
+		xpath = "//input[@value='Deallocate']";
+		if(!ta.elementExist(xpath))
+		{
+			xpath = "//input[@value='Find']";
+			passed = ta.clickOn(xpath);
+			
+			xpath = "//input[@value='Deallocate']";
+			passed = ta.waitUntil(xpath, 5);
+		}
+		
+		xpath = "(//*[text()[contains(.,'Service Usage:')]]/./following::select)[1]";
+		passed = ta.selectBy(xpath, "V");
+		
+		xpath = "(//*[text()[contains(.,'Charge Option:')]]/./following::select)[1]";
+		passed = ta.selectBy(xpath, "2");
+		
+		xpath = "(//*[text()[contains(.,'Special Instructions:')]]/./following::textArea)[1]";
+		passed = ta.sendDatatoField(xpath, "Automation Testing");
+		
+		report.takeScreenshot();
+		
+		xpath = "(//*[text()[contains(.,'Same as Account Address:')]]/./following::input)[1]";
+		passed = ta.clickOn(xpath);
+		
+		xpath = "(//*[text()[contains(.,'Address Type:')]]/./following::select)[1]";
 		passed = ta.waitUntilElementnotExist(xpath, 5);
 		
 		return passed;
