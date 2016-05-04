37cffb819dda5b3636286a1ad017113ddc828d14
Nikhil Das <nikhil.thottupurathu@igate.com>
2016-05-04 13:38:24 +0530

    Edit for testing

--- Automation_Suite_V2/src/main/MainClass.java
+++ Automation_Suite_V2/src/main/MainClass.java
@@ -1,5 +1,6 @@
 package main;
 
+import operations.TestReport;
 import testcase.ProvideService;
 
 public class MainClass
@@ -8,8 +9,12 @@ public class MainClass
 	{
 //		CreateNewAccount createNewAccount = new CreateNewAccount("fixedResidential","ETFTESTING");
 //		createNewAccount.execute();
-		
-		ProvideService provideService = new ProvideService(false);
+
+		TestReport report = new TestReport("ProvideService", "Reports\\");
+
+		ProvideService provideService = new ProvideService(report, "240004430000");
 		provideService.execute();
+		report.createScreenshotDocument();
+
 	}
 }
