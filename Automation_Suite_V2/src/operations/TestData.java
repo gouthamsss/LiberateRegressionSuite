6cd11eb1ac693b7fe5a0e8d3f4783c7f559cd607
Nikhil Das <nikhil.thottupurathu@igate.com>
2016-05-05 14:07:18 +0530

    TestData 0.1.3 : Added: Database Connectivity

--- Automation_Suite_V2/src/operations/TestData.java
+++ Automation_Suite_V2/src/operations/TestData.java
@@ -1,19 +1,84 @@
 package operations;
 
+import java.io.File;
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class TestData
 {
-	String testDataClassVersion = "0.1.1 : Amended : Method nam e to generate random string";
+	public static String testDataClassVersion = "TestData 0.1.3 : Added: Database Connectivity";
 	
-	public static SecureRandom random = new SecureRandom();
+	public static SecureRandom random	= new SecureRandom();
+	public static TestAction ta			= new TestAction(Operations.getdriver());
 	
+	//Save and Retrieve Data
+	public static boolean validateDBFile()
+	{
+		File dbFile				= new File("\\Data\\data.db");
+	    Statement statement		= null;
+	    Connection connection	= null;
+	    
+		if(!dbFile.exists())
+		{
+			try
+			{
+				dbFile.createNewFile();
+			}
+			catch (Exception e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+	    try
+	    {
+	    	connection = DriverManager.getConnection("jdbc:sqlite:data.db");
+		    ta.log("Opened database successfully");
+		}
+	    catch (SQLException e)
+	    {
+			// TODO Auto-generated catch block
+	    	ta.log("ERROR : Not able to connect to local database. Please contact support with log");
+			e.printStackTrace();
+		}
+	    
+	    if(!connection.equals(null))
+	    {
+	        try
+	        {
+				statement = connection.createStatement();
+			}
+	        catch (SQLException e)
+	        {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	    }
+		
+		
+		return true;
+	}
+	
+	//Data Generation
 	public static String randomString()
 	{
 		String customerName = new BigInteger(130, random).toString(32);
 		customerName = customerName.substring(0,12);
 		
 		return customerName;
+	}
+	public static long  randomNumeric()
+	{
+		long max = 9999999999L;
+		max = (long) (Math.random() * ((max - 1) + 1));
+		
+		return max;
 	}
 }
