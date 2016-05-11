02149965d15b1bf742e00a493bc7367a47175941
Nikhil Das <nikhil.thottupurathu@igate.com>
2016-05-11 17:00:19 +0530

    Added Addmore

--- Automation_Suite_V2/src/testcase/SalesSignoff.java
+++ Automation_Suite_V2/src/testcase/SalesSignoff.java
@@ -41,10 +41,34 @@ public class SalesSignoff
 		xpath = "(//*[text()[contains(.,'Service Order No:')]]/./following::*)[1]";
 		this.ServiceOrder = ta.getDatafromPage(xpath);
 		
+		//Check for Appointment Detail
+		xpath = "(//*[text()[contains(.,'Municipality Code:')]]/./following::select[@class='iceSelOneMnu MandatoryListBox'])[1]";
+		if(ta.elementExist(xpath))
+		{
+			passed = ta.selectBy(xpath, 1);
+			ta.waitFor(1000);
+		}
+		
 		xpath = "//input[@value='Confirm Payment Amounts' and @disabled='disabled']";
 		if(!ta.elementExist(xpath))
 		{
 			//TODO Confirm Payments
+			xpath = "(//input[@type='checkbox' and @checked='checked'])";
+			for(int i = 1; i<5; i++)
+			{
+				if(ta.elementExist(xpath+"["+i+"]"))
+				{
+					ta.clickOn(xpath+"["+i+"]");
+				}
+				else
+				{
+					break;
+				}
+			}
+			xpath = "//input[@value='Confirm Payment Amounts']";
+			passed = ta.clickOn(xpath);
+			ta.waitFor(1000);
+			ta.closeOKpopup();				
 		}
 		
 		//TODO Handle if service charge needs to be added.
@@ -64,7 +88,7 @@ public class SalesSignoff
 		return passed;
 	}
 	
-	public boolean addMore()
+	public boolean addMore(String serviceType)
 	{
 		xpath = "//*[text()='Service Order Lines']";
 		passed = ta.waitUntil(xpath, 10);
@@ -90,30 +114,33 @@ public class SalesSignoff
 			ta.waitFor(1000);
 		}
 		
-		xpath = "//input[@value='Confirm Payment Amounts' and @disabled='disabled']";
-		if(!ta.elementExist(xpath))
-		{
-			//TODO Confirm Payments
-			xpath = "//input[@type='checkbox' and @checked='checked']";
-			for(int i = 0; i<5; i++)
-			{
-				if(ta.elementExist(xpath))
-				{
-					ta.clickOn(xpath);
-				}
-				else
-				{
-					break;
-				}
-			}
-		}
-		
 		//TODO Handle if service charge needs to be added.
 		
 		xpath = "//input[@value='Add More']";
 		passed = ta.clickOn(xpath);
 		report.takeScreenshot();
 		
+		xpath = "//*[text()='Change Service']";
+		passed = ta.waitUntil(xpath, 5);
+		
+		if(serviceType.equals("PDL"))
+		{
+			xpath = "//*[text()='ADSL']";
+			if(!ta.elementExist(xpath))
+			{
+				xpath = "(//*[text()[contains(.,'PEL')]])[1]";
+				ta.clickOn(xpath);
+			}
+			xpath = "//*[text()='ADSL']";
+			passed = ta.waitUntil(xpath, 2);
+			passed = ta.clickOn(xpath);
+		}
+		else
+		{
+			xpath = "//*[text()='Service Package']";
+			passed = ta.waitUntil(xpath, 2);
+			passed = ta.clickOn(xpath);
+		}
 		return passed;
 	}
 }
