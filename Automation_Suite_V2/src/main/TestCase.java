a0a9a5c53de5c45e36ce580742c345d65c5d18a7
Nikhil Das <nikhil.thottupurathu@igate.com>
2016-05-25 15:16:19 +0530

    Added New Test Case

--- Automation_Suite_V2/src/main/TestCase.java
+++ Automation_Suite_V2/src/main/TestCase.java
@@ -1,12 +1,13 @@
 package main;
 
 import operations.TestReport;
-import testcase.CreateDepositReason;
 import testcase.CreateNewAccount;
 import testcase.CustomerSearch;
 import testcase.LoginLogout;
+import testcase.MaintainAccountDeposit;
 import testcase.Payments;
 import testcase.ProvideService;
+import testcase.Routing;
 
 public class TestCase 
 {
@@ -18,9 +19,9 @@ public class TestCase
 
 		TestReport report = new TestReport("PELExistingCustomer", ReportLocation);
 
-		ProvideService provideService = new ProvideService(report, "240004430000");
-		provideService.ServiceType = "PEL";
-		provideService.ServicePackage = "ETFTESTING";
+		ProvideService provideService	= new ProvideService(report, "240004430000");
+		provideService.ServiceType		= "PEL";
+		provideService.ServicePackage	= "ETFTESTING";
 		provideService.execute();
 		report.createScreenshotDocument();
 	}
@@ -31,9 +32,9 @@ public class TestCase
 
 		TestReport report = new TestReport("PDLExistingCustomer", ReportLocation);
 
-		ProvideService provideService = new ProvideService(report, "240004430000");
-		provideService.ServiceType = "PDL";
-		provideService.ServicePackage = "ADSL";
+		ProvideService provideService	= new ProvideService(report, "240004430000");
+		provideService.ServiceType		= "PDL";
+		provideService.ServicePackage	= "ADSL";
 		provideService.execute();
 		report.createScreenshotDocument();
 	}
@@ -43,16 +44,17 @@ public class TestCase
 		LoginLogout.login(MainClass.AUTURL);
 
 		TestReport report = new TestReport("PELPDLNewCustomer", ReportLocation);
-		CreateNewAccount createNewAccount = new CreateNewAccount(report, "fixedResidential","ETFTESTING");
-		createNewAccount.ServiceType = "PEL";
-		createNewAccount.ServicePackage = "ETFTESTING";
+		
+		CreateNewAccount createNewAccount	= new CreateNewAccount(report, "fixedResidential","ETFTESTING");
+		createNewAccount.ServiceType		= "PEL";
+		createNewAccount.ServicePackage		= "ETFTESTING";
 		createNewAccount.execute();
 
 		ProvideService provideService		= new ProvideService(report, createNewAccount.AccountNumber);
 		provideService.NewCustomer			= true;
 		provideService.PELPDLProvisioning	= true;
-		provideService.ServiceType = createNewAccount.ServiceType;
-		provideService.ServicePackage = createNewAccount.ServicePackage;
+		provideService.ServiceType			= createNewAccount.ServiceType;
+		provideService.ServicePackage		= createNewAccount.ServicePackage;
 		provideService.execute();
 		report.createScreenshotDocument();
 	}
@@ -62,10 +64,11 @@ public class TestCase
 		LoginLogout.login(MainClass.AUTURL);
 
 		TestReport report = new TestReport("PCLExistingCustomer", ReportLocation);
-		ProvideService provideService = new ProvideService(report, "240004430000");
-		provideService.NewCustomer = false;
-		provideService.ServiceType = "PCL";
-		provideService.ServicePackage = "PCL_Res";
+		
+		ProvideService provideService	= new ProvideService(report, "240004430000");
+		provideService.NewCustomer		= false;
+		provideService.ServiceType		= "PCL";
+		provideService.ServicePackage	= "PCL_Res";
 		provideService.execute();
 		
 		report.createScreenshotDocument();
@@ -76,37 +79,39 @@ public class TestCase
 		LoginLogout.login(MainClass.AUTURL);
 
 		TestReport report = new TestReport("PCLNewCustomer", ReportLocation);
-		CreateNewAccount createNewAccount = new CreateNewAccount(report, "fixedResidential","PCL_Res");
-		createNewAccount.ServiceType = "PCL";
-		createNewAccount.ServicePackage = "PCL_Res";
+		
+		CreateNewAccount createNewAccount	= new CreateNewAccount(report, "fixedResidential","PCL_Res");
+		createNewAccount.ServiceType		= "PCL";
+		createNewAccount.ServicePackage		= "PCL_Res";
 		createNewAccount.execute();
 
-		ProvideService provideService = new ProvideService(report, createNewAccount.AccountNumber);
-		provideService.NewCustomer = true;
-		provideService.ServiceType = createNewAccount.ServiceType;
-		provideService.ServicePackage = createNewAccount.ServicePackage;
+		ProvideService provideService		= new ProvideService(report, createNewAccount.AccountNumber);
+		provideService.NewCustomer			= true;
+		provideService.ServiceType			= createNewAccount.ServiceType;
+		provideService.ServicePackage		= createNewAccount.ServicePackage;
 		provideService.execute();
 		report.createScreenshotDocument();
 	}
 	
 	public static void PCLNewCustomerwithCreditLimit()
 	{
-		MainClass.AUTURL = "http://172.21.73.80:8081/liberate-LONI01-S10/";
-		MainClass.Environment = "S10";
+		MainClass.AUTURL		= "http://172.21.73.80:8081/liberate-LONI01-S10/";
+		MainClass.Environment	= "S10";
 				
 		LoginLogout.login(MainClass.AUTURL);
 
 		TestReport report = new TestReport("PCLNewCustomerwithCreditLimit", ReportLocation);
-		CreateNewAccount createNewAccount = new CreateNewAccount(report, "fixedResidential","PCL_Res");
-		createNewAccount.ServiceType = "PCL";
-		createNewAccount.ServicePackage = "PCL_Res";
+		
+		CreateNewAccount createNewAccount	= new CreateNewAccount(report, "fixedResidential","PCL_Res");
+		createNewAccount.ServiceType		= "PCL";
+		createNewAccount.ServicePackage		= "PCL_Res";
 		createNewAccount.execute();
 
-		ProvideService provideService = new ProvideService(report, createNewAccount.AccountNumber);
-		provideService.NewCustomer = true;
-		provideService.ServiceType = createNewAccount.ServiceType;
-		provideService.ServicePackage = createNewAccount.ServicePackage;
-		provideService.createCreditLimit = true;
+		ProvideService provideService		= new ProvideService(report, createNewAccount.AccountNumber);
+		provideService.NewCustomer			= true;
+		provideService.ServiceType			= createNewAccount.ServiceType;
+		provideService.ServicePackage		= createNewAccount.ServicePackage;
+		provideService.createCreditLimit	= true;
 		provideService.execute();
 		report.createScreenshotDocument();
 	}
@@ -118,9 +123,9 @@ public class TestCase
 
 		TestReport report = new TestReport("PTVExisting", ReportLocation);
 
-		ProvideService provideService = new ProvideService(report, "240004430000");
-		provideService.ServiceType = "PTV";
-		provideService.ServicePackage = "PTV_RES";
+		ProvideService provideService		= new ProvideService(report, "240004430000");
+		provideService.ServiceType			= "PTV";
+		provideService.ServicePackage		= "PTV_RES";
 		provideService.execute();
 		report.createScreenshotDocument();
 	}
@@ -131,9 +136,9 @@ public class TestCase
 
 		TestReport report = new TestReport("PTIExisting", ReportLocation);
 
-		ProvideService provideService = new ProvideService(report, "260000550000");
-		provideService.ServiceType = "PTI";
-		provideService.ServicePackage = "IPTVBasicS";
+		ProvideService provideService	= new ProvideService(report, "260000550000");
+		provideService.ServiceType		= "PTI";
+		provideService.ServicePackage	= "IPTVBasicS";
 		provideService.execute();
 		report.createScreenshotDocument();
 	}
@@ -194,6 +199,7 @@ public class TestCase
 		LoginLogout.login(MainClass.AUTURL);
 
 		TestReport report = new TestReport("PCLComverseExistingCustomer", ReportLocation);
+		
 		ProvideService provideService	= new ProvideService(report, "240004430000");
 		provideService.NewCustomer		= false;
 		provideService.ServiceType		= "PCL";
@@ -212,8 +218,9 @@ public class TestCase
 	{
 		LoginLogout.login(MainClass.AUTURL);
 
-		TestReport report = new TestReport("BatchPayment", ReportLocation);
-		Payments payment = new Payments(report, "Batch");
+		TestReport report	= new TestReport("BatchPayment", ReportLocation);
+		
+		Payments payment	= new Payments(report, "Batch");
 		payment.execute();
 		
 		report.createScreenshotDocument();
@@ -223,10 +230,11 @@ public class TestCase
 	{
 		LoginLogout.login(MainClass.AUTURL);
 
-		TestReport report = new TestReport("SinglePayment", ReportLocation);
-		Payments payment = new Payments(report, "SinglePayment");
-		payment.AccountNumber = "210010230000";
-		payment.SinglePaymentType = "Standard";
+		TestReport report			= new TestReport("SinglePayment", ReportLocation);
+		
+		Payments payment			= new Payments(report, "SinglePayment");
+		payment.AccountNumber		= "210010230000";
+		payment.SinglePaymentType	= "Standard";
 		payment.execute();
 		
 		report.createScreenshotDocument();
@@ -236,8 +244,9 @@ public class TestCase
 	{
 		LoginLogout.login(MainClass.AUTURL);
 
-		TestReport report = new TestReport("VerifyCashdrawer", ReportLocation);
-		Payments payment = new Payments(report, "CashDrawer");
+		TestReport report	= new TestReport("VerifyCashdrawer", ReportLocation);
+		
+		Payments payment	= new Payments(report, "CashDrawer");
 		payment.execute();
 		
 		report.createScreenshotDocument();
@@ -247,13 +256,36 @@ public class TestCase
 	{
 		LoginLogout.login(MainClass.AUTURL);
 
-		TestReport report = new TestReport("CreateDepositReason", ReportLocation);
-		CustomerSearch customerSearch = new CustomerSearch(report);
-		customerSearch.AccountNumber = "240004430000";
+		TestReport report 				= new TestReport("CreateDepositReason", ReportLocation);
+		
+		CustomerSearch customerSearch	= new CustomerSearch(report);
+		customerSearch.AccountNumber 	= "240004430000";
 		customerSearch.execute();
 		
-		CreateDepositReason depositreason = new CreateDepositReason(report);
+		MaintainAccountDeposit depositreason = new MaintainAccountDeposit(report);
 		depositreason.execute();
+		
+		Payments depositpayment			= new Payments(report, "SinglePayment");
+		depositpayment.AccountNumber	= customerSearch.AccountNumber;
+		depositpayment.SinglePaymentType= "Deposit";
+		depositpayment.execute();
+		
+		report.createScreenshotDocument();
+	}
+	
+	public static void FibreRouting()
+	{
+		LoginLogout.login(MainClass.AUTURL);
+
+		TestReport report		= new TestReport("FibreRouting", ReportLocation);
+		
+		Routing routing			= new Routing(report, "Auto");
+		routing.ServiceOrder	= "XD00203";
+		routing.Department		= "BILDM";
+		routing.PlantItemType	= "FDP";
+		routing.PlantItemNum	= "FDP20";
+		routing.ExchnageID		= "BOT";
+		routing.execute();
 		
 		report.createScreenshotDocument();
 	}
